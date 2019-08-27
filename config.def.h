95c138adb5d11269f3d79920835e040f9325e3b1
Storm <stormvansoldt94@gmail.com>
2019-08-27 01:45:27 -0600

    adding personal configuration

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "dejavu sans mono:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -84,31 +84,31 @@ unsigned int tabspaces = 8;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
+	[0] = "#000000", /* black   */
+	[1] = "#ff615a", /* red     */
+	[2] = "#b1e969", /* green   */
+	[3] = "#ebd99c", /* yellow  */
+	[4] = "#5da9f6", /* blue    */
+	[5] = "#e86aff", /* magenta */
+	[6] = "#82fff7", /* cyan    */
+        [7] = "#dedacf", /* white   */
+	
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	[8]  = "#313131", /* black   */
+	[9]  = "#f58c80", /* red     */
+	[10] = "#ddf88f", /* green   */
+	[11] = "#eee5b2", /* yellow  */
+	[12] = "#a5c7ff", /* blue    */
+	[13] = "#ddaaff", /* magenta */
+	[14] = "#b7fff9", /* cyan    */
+	[15] = "#ffffff", /* white   */
 
-	[255] = 0,
+	/* special colors */
+	[256] = "#171717", /* background */
+	[257] = "#dedacf", /* foreground */
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
 };
 
 
@@ -116,10 +116,17 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
+
+/*
+ * Colors used when the specific fg == defaultfg. So in reverse mode this will
+ * reverse too. Another logic would only make the simple feature too complex.
+ */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
 
 /*
  * Default shape of cursor

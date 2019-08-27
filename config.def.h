beb0d241c63bb657b17c6bf51fddb47cd7e19d77
Storm <stormvansoldt94@gmail.com>
2019-08-27 04:54:06 -0600

    Added Wombat theme, UbuntuMono Nerd Font, and the anysize patch

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "dejavu sans mono:pixelsize=14:antialias=true:autohint=true";
+static char *font = "UbuntuMono Nerd Font Mono:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
